c9403633be95aee55007a3448b0515d6a054176f
Jilomena <santiagoanajimena@gmail.com>
2026-05-04 09:10:12 -0600

    cambios

--- submenu.c
+++ submenu.c
@@ -15,7 +15,9 @@ void mostrarDato(struct Dato *ptr);
 void liberarDato(struct Dato **ptr);
 void liberarTodo(struct Dato **ptr);
 void buscardato(struct Dato *ptr);
-void apuntardato(struct Dato **ptr);
+void contardato(struct Dato **ptr);
+void remplazarDato(struct Dato **ptr);
+void ordenarDato(struct Dato **ptr);
 
 //meun principal 
 int menu(void){
@@ -36,8 +38,10 @@ int submenu(void){
 
     printf("submenu de funciones \n");
     printf("1.- buscar dato\n");
-    printf("2.- apuntar dato\n");
-    printf("3.- regresar\n");
+    printf("2.- contar dato\n");
+    printf("3.- remplazar dato\n");
+    printf("4.- ordenar dato\n");
+    printf("5.- regresar\n");
     scanf("%d",&opcion);
 
     return opcion;
@@ -52,7 +56,6 @@ int main(void){
         opcion = menu();
 
         switch(opcion){
-
             case 1:
                 ptrTemp = crearDato();
 
@@ -82,31 +85,41 @@ int main(void){
                     switch(subopcion){
 
                         case 1:
-                            printf("caso one");
+                            buscarDato(ptr);
                             break;
 
                         case 2:
-                            printf("caso two");
+                            remplazarDato(&ptr);
                             break;
 
                         case 3:
+                            contarnodo(&ptr);
+                            break;
+
+                        case 4:
                             printf("Regresando al menu principal...\n");
                             break;
 
+                        default:
+                            printf("Opcion invalida.\n");
                     }
 
-                }while(subopcion != 3);
+                }while(subopcion != 4);
                 break;
 
             case 3:
-                printf("Saliendo...\n");
+                liberarDato(&ptr);
                 break;
-
+            case 4:
+                liberarTodo(&ptr); // NUEVA FUNCION CLASE
+                break;
+            case 5:
+                break;
             default:
                 printf("Opcion invalida.\n");
         }
 
-    }while(opcion != 3);
+    }while(opcion != 5);
 
     return 0;
 }
@@ -189,3 +202,47 @@ void liberarTodo(struct Dato **ptr){
 
     printf("Todos los datos fueron liberados.\n");
 }
+
+void buscarDato(struct Dato *ptr){
+    int valor, encontrado = 0 // esta en una bandera e inica en cero 
+
+    printf("Ingrese el dato a buscar: ");
+    scanf("%d", &valor);
+
+    while(ptr != NULL){ }// aqui se recorre la lista el siclo sigue no importa que tenga nodos 
+
+        if(ptr->d == valor){ // aqui es donde se compara el dato del nodo (ptr ->d) con el valor que se esta buscando 
+            printf("Dato encontrado: %d\n", ptr->d);
+            encontrado = 1;  // si son iguales cambia el encontado a 1
+        }
+        
+        ptr = ptr->ptrSig; // este es para avanzar al sigiente nodos 
+    }
+
+    if(encontrado == 0){
+        printf("Dato no encontrado.\n");
+    }
+
+void remplazarDato(struct Dato **ptr){
+    int valor, nuevo; // nuevo es para el nuevo valor del que vamos a remplazar 
+
+    struct Dato *aux = *ptr;
+
+    printf("Ingrese el dato que quiere reemplazar: ");
+    scanf("%d", &valor);
+
+    printf("Ingrese el nuevo dato: ");
+    scanf("%d", &nuevo);
+
+    while(aux != NULL){  }  //recorremo la lista  
+        if(aux->d == valor){ // se selecciona al nodo de el valor que vamos a cambiar 
+            aux->d = nuevo; // remplazamos por el nuevo 
+            printf("Dato reemplazado.\n");
+            return;
+        }
+        aux = aux->ptrSig; // avanzamos al siguiente nodo 
+    }
+
+    printf("Dato no encontrado.\n");
+
+
