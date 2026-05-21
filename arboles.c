7019509f2760f49cf7e1d8396b5f6f4d601e832d
Ana81194 <santiagoanajimena@gmail.com>
2026-05-21 13:30:54 -0600

    arboles

--- arboles.c
+++ arboles.c
@@ -1,48 +1,112 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// Estructura del árbol
 struct arbol{
     int dato;
-    struct arbol *izq , *der;
-    raiz=NULL;
-}
+    struct arbol *izq, *der;
+};
+
+// Prototipos
+int menu();
+struct arbol* crearnodo();
+struct arbol* insertar(struct arbol *raiz, struct arbol *nuevo);
+
+// Menú
 int menu() {
+
     int opcion;
-    printf("\nMenú\n");
-    printf("1.- insertar\n");
-    printf("2.- salir\n");
+
+    printf("\nMENU\n");
+    printf("1.- Insertar\n");
+    printf("2.- Salir\n");
+    printf("Opcion: ");
     scanf("%d", &opcion);
+
     return opcion;
 }
 
-int main(void){
-    struct *raiz;
-    do {
-        opcion = menu();
-        switch (opcion) {
-            case 1:
-            ptrtemp= crearnodo();
-            if(ptrtemp=NULL){
-                printf("no se pudo crear el nodo\n");
-            }else{
-                printf("el nodo fue creado exitosamente\n");
-                if(raiz==NULL)
-                    raiz= ptrtemp;
-            }
-                while{
-                    if(nuevo->dato < raiz->dato){
-                    raiz->izq = insertar(raiz->izq, nuevo);
-                }
-                     else if(nuevo->dato > raiz->dato){
-                        raiz->der = insertar(raiz->der, nuevo);
-                     }
+// Crear nodo
+struct arbol* crearnodo(){
 
-                     return raiz;
+    struct arbol *ptrtemp;
+
+    ptrtemp = (struct arbol*) malloc(sizeof(struct arbol));
+
+    if(ptrtemp != NULL){
+
+        printf("Ingrese el dato: ");
+        scanf("%d", &ptrtemp->dato);
+
+        ptrtemp->izq = NULL;
+        ptrtemp->der = NULL;
+    }
+
+    return ptrtemp;
+}
+
+// Insertar nodo
+struct arbol* insertar(struct arbol *raiz, struct arbol *nuevo){
+
+    if(raiz == NULL){
+
+        return nuevo;
+    }
+
+    if(nuevo->dato < raiz->dato){
+
+        raiz->izq = insertar(raiz->izq, nuevo);
+
+    }else if(nuevo->dato > raiz->dato){
+
+        raiz->der = insertar(raiz->der, nuevo);
+    }
+
+    return raiz;
+}
+
+int main(void){
+
+    struct arbol *raiz = NULL;
+    struct arbol *ptrtemp;
+
+    int opcion;
+
+    do {
+
+        opcion = menu();
+
+        switch (opcion) {
+
+            case 1:
+
+                ptrtemp = crearnodo();
+
+                if(ptrtemp == NULL){
+
+                    printf("No se pudo crear el nodo\n");
+
+                }else{
+
+                    printf("El nodo fue creado exitosamente\n");
+
+                    raiz = insertar(raiz, ptrtemp);
                 }
+
                 break;
+
             case 2:
-                salir
+
+                printf("Saliendo...\n");
+
                 break;
+
+            default:
+
+                printf("Opcion no valida\n");
         }
+
     } while (opcion != 2);
 
     return 0;
-}  este es mi codigo 
+}
